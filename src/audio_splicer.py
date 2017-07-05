39f99aacb9928a8b5c9da07fcb696a8b9ffb78ee
Ayush Singh <ayushsinghiii.94@gmail.com>
2017-07-05 20:03:48 +0530

    modified as required

--- src/audio_splicer.py
+++ src/audio_splicer.py
@@ -9,10 +9,10 @@ try:
     aud_src = sys.argv[1]
     trans = sys.argv[2]
 except Exception:
-    aud_src = 'recording.mp3'
+    aud_src = 'recording.flac'
     trans = 'transcript.csv'
     print("No arguments! switched to default!")
-sound = AudioSegment.from_mp3(aud_src)
+sound = AudioSegment.from_file(aud_src)
 
 if not os.path.exists('clips'):
     print('Directory doesn\'t exist, creating!')
@@ -20,14 +20,14 @@ if not os.path.exists('clips'):
 
 with open(trans) as csv_file:
     reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-    i = 0
     for item in reader:
         start = float(item[0].split('s')[0])
         stop = float(item[1].split('s')[0])
-        start *= 1000
-        stop *= 1000
+        start = (start - 3)*1000 if start > 0 else 0
+        #print((start))
+        stop =  (stop + 3)*1000 if stop < len(sound) else len(sound)
+        #print(stop)
         splr = sound[start:stop]
-        i += 1
-        name = 'clips/file' + str(i) + '.mp3'
+        name = 'clips/aud ' + str(start/1000) + ' ' + str(stop/1000) + '.mp3'
         print('created file: ' + name)
         splr.export(name, format="mp3")
